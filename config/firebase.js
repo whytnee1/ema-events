8e6081b58cd1efb6a797e2508c393c8f13737c89
Njoagwuali Destiny <witneyd.da@gmail.com>
2026-09-02 16:40:12 +0100

    second update

---
+++ config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbaAH0dqTL5UJ1cfpjOM44AuPym9pX360",
+  authDomain: "ema-events-c9187.firebaseapp.com",
+  projectId: "ema-events-c9187",
+  storageBucket: "ema-events-c9187.firebasestorage.app",
+  messagingSenderId: "911000107017",
+  appId: "1:911000107017:web:5fbb665b335f4b534be0d8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
